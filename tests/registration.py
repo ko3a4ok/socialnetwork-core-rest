64e826e6237519c542cfdca2cd2eafa65354a319
roma <ko3a4ok.ua@gmail.com>
2015-04-26 01:34:19 +0300

    [ADD] tests for registration and login

---
+++ tests/registration.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+import unittest
+import uuid
+
+import core
+
+
+__author__ = 'ko3a4ok'
+
+
+class UserRegistration(TestCase):
+    def __init__(self, *a, **m):
+        super(UserRegistration, self).__init__(*a, **m)
+        self.email = uuid.uuid4()
+        self.password = uuid.uuid4()
+
+    def setUp(self):
+        core.app.config['TESTING'] = True
+        self.app = core.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_create_new_user(self):
+        data = dict(
+            email=self.email,
+            password=self.password)
+        res = self.app.post('/user/register', data=data)
+        assert res.status_code == 200
+
+    def test_login_user(self):
+        self.test_create_new_user()
+        data = dict(
+            email=self.email,
+            password=self.password)
+        res = self.app.post('/user/login', data=data)
+        assert res.status_code == 200
+        self.cookie = self.app.cookie_jar._cookies['localhost.local']['/']['session'].value
+
+    def test_user_me(self):
+        self.test_login_user()
+        self.app.set_cookie('localhost.local', 'session')
+        res = self.app.get('/user/me')
+        assert res.status_code == 401
+        self.app.set_cookie('localhost.local', 'session', value=self.cookie)
+        res = self.app.get('/user/me')
+        assert res.status_code == 200
+
+
+if __name__ == '__main__':
+    unittest.main()
