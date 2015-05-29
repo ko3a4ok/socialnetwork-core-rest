35dc345142ca6a2857b6578339e53e26f007269a
roma <ko3a4ok.ua@gmail.com>
2015-05-29 16:39:24 +0300

    [ADD] #8 social feed

--- core/posts.py
+++ core/posts.py
@@ -5,6 +5,7 @@ import flask
 from flask.ext.login import login_required, current_user
 import time
 import core
+from core.follow import FOLLOWING
 
 __author__ = 'ko3a4ok'
 
@@ -25,7 +26,8 @@ ERROR_POST_NOT_FOUND = 'Sorry, but post not found'
 def post():
     new_params = json.loads(request.data.decode('utf8'))
     new_params[CREATED_AT] = time.time()
-    result = mongo.db[POSTS + "_" + current_user.id].insert(new_params)
+    new_params[CREATED_BY] = bson.ObjectId(current_user.id)
+    result = mongo.db[POSTS].insert(new_params)
     return bson.json_util.dumps({'_id': result})
 
 
@@ -33,7 +35,7 @@ def post():
 @login_required
 def get_post(user_id, post_id):
     post_id = bson.ObjectId(post_id)
-    r = mongo.db[POSTS + "_" + user_id].find_one({'_id': post_id})
+    r = mongo.db[POSTS].find_one({'_id': post_id})
     if not r:
         return flask.make_response(ERROR_POST_NOT_FOUND, 404)
     r[CREATED_BY] = core.users.get_user_fast(user_id)
@@ -46,7 +48,7 @@ def update_post(post_id):
     post_id = bson.ObjectId(post_id)
     new_params = json.loads(request.data.decode('utf8'))
     new_params[UPDATED_AT] = time.time()
-    r = mongo.db[POSTS + "_" + current_user.id].update({'_id': post_id}, {'$set': new_params})
+    r = mongo.db[POSTS].update({'_id': post_id, CREATED_BY: bson.ObjectId(current_user.id)}, {'$set': new_params})
     if not r['updatedExisting']:
         return flask.make_response(ERROR_POST_NOT_FOUND, 404)
     return bson.json_util.dumps({'_id': post_id})
@@ -56,7 +58,7 @@ def update_post(post_id):
 @login_required
 def delete_post(post_id):
     post_id = bson.ObjectId(post_id)
-    r = mongo.db[POSTS + "_" + current_user.id].remove({'_id': post_id}, True)
+    r = mongo.db[POSTS].remove({'_id': post_id, CREATED_BY: bson.ObjectId(current_user.id)}, True)
     if not r['n']:
         return flask.make_response(ERROR_POST_NOT_FOUND, 404)
     return '{}'
@@ -68,16 +70,26 @@ def timeline(user_id):
     user = get_user(user_id)
     if not user:
         return flask.make_response(ERROR_USER_NOT_FOUND, 404)
-    t = POSTS + "_" + user_id
     limit = int(request.args.get('limit', 10))
     offset = int(request.args.get('offset', 0))
-    cnt = mongo.db[t].count()
-    skip = cnt-offset-limit
-    if skip < 0:
-        limit += skip
-        skip = 0
-    r = mongo.db[t].find().skip(skip).limit(limit)
-    posts = reversed(list(r))
+    uid = bson.ObjectId(current_user.id)
+    r = mongo.db[POSTS].find({CREATED_BY: uid}).sort([(CREATED_AT, -1)]).skip(offset).limit(limit)
+    posts = list(r)
     for post in posts:
         post[CREATED_BY] = user
+    return bson.json_util.dumps({'posts': posts})
+
+
+@app.route('/feed')
+@login_required
+def feed():
+    limit = int(request.args.get('limit', 10))
+    offset = int(request.args.get('offset', 0))
+    uid = bson.ObjectId(current_user.id)
+    f = mongo.db.users.find_one({'_id': uid}, {'_id': 0, FOLLOWING: 1})
+    following = f[FOLLOWING] + [uid]
+    r = mongo.db[POSTS].find({CREATED_BY: {"$in": following}}).sort([(CREATED_AT, -1)]).skip(offset).limit(limit)
+    posts = list(r)
+    for post in posts:
+        post[CREATED_BY] = get_user(str(post[CREATED_BY]))
     return bson.json_util.dumps({'posts': posts})
