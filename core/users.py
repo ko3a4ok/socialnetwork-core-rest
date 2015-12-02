0bd2937f94c8681b7b6b8d00d07e78ca210fe3fc
roma <ko3a4ok.ua@gmail.com>
2015-12-03 00:49:52 +0200

    [ADD] #19 Facebook integration

--- core/users.py
+++ core/users.py
@@ -6,26 +6,32 @@ from flask import request
 import flask
 from flask.ext.login import login_user, current_user, UserMixin, login_required
 import bson.objectid
+import requests
 import core.utils
 from core import app, login_manager, redis, search, SEARCH_INDEX
 from core import mongo
 from flask.ext.cors import cross_origin
 
 
+ACCESS_TOKEN = "access_token"
+FACEBOOK_ID = "facebook_id"
 EMAIL = 'email'
 PASSWORD = 'password'
 NAME = 'name'
 FOLLOWERS = 'followers'
 FOLLOWING = 'following'
-PROPERTIES = {PASSWORD: 0, FOLLOWING: 0, FOLLOWERS : 0}
+PROPERTIES = {PASSWORD: 0, FOLLOWING: 0, FOLLOWERS: 0}
+ERROR_FACEBOOK_ACCESS_TOKEN = 'Please provide access token'
 ERROR_USER_EXISTS = 'Sorry, user with this email has already registered'
 ERROR_WRONG_CREDENTIALS = 'Sorry, wrong credentials'
 ERROR_USER_NOT_FOUND = 'Sorry, user not found'
 FAST_USER_PROPERTIES = ['_id', 'name', 'mini_profile_url']
 pool = ThreadPool(processes=5)
 
+
 def support_jsonp(f):
     """Wraps JSONified output for JSONP"""
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         callback = request.args.get('callback', False)
@@ -34,7 +40,9 @@ def support_jsonp(f):
             return app.response_class(content, mimetype='application/json')
         else:
             return f(*args, **kwargs)
+
     return decorated_function
+
 
 class User(UserMixin):
     def __init__(self, u):
@@ -53,6 +61,36 @@ def register():
     login_user(User(user))
     return bson.json_util.dumps(user)
 
+
+@app.route("/user/facebook", methods=['POST'])
+def facebook():
+    host = "https://graph.facebook.com/v2.5"
+    path = "/me?fields=id,name,birthday,gender"
+    access_token = request.form[ACCESS_TOKEN]
+    if not access_token: return flask.make_response(ERROR_FACEBOOK_ACCESS_TOKEN, 401)
+    url = '{host}{path}&access_token={access_token}'.format(host=host, path=path, access_token=access_token)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return flask.make_response(response.content, 401)
+    user = json.loads(response.content)
+
+    cur = mongo.db.users.find({FACEBOOK_ID: user['id']}, {PASSWORD: 0})
+    if not cur.count():
+        if 'gender' in user:
+            user['sex'] = user.pop('gender')
+        if 'birthday' in user:
+            bparts = user['birthday'].split('/')
+            user['birthday'] = '/'.join(bparts[1::-1] + bparts[2:])
+        photo_url = host + "/me/picture?type=large&access_token=" + access_token + "&redirect=false"
+        mini_photo_url = host + "/me/picture?type=square&access_token=" + access_token + "&redirect=false"
+        user[FACEBOOK_ID] = user.pop('id')
+        user['profile_url'] = requests.get(photo_url).json()['data']['url']
+        user['mini_profile_url'] = requests.get(mini_photo_url).json()['data']['url']
+        user[PASSWORD] = None
+        mongo.db.users.insert(user)
+    user = cur[0]
+    login_user(User(user))
+    return bson.json_util.dumps(user)
 
 @app.route('/user/login', methods=['POST'])
 @cross_origin(supports_credentials=True)
@@ -102,7 +140,7 @@ def user_profile(id):
         ar = mongo.db.users.aggregate([{'$match': q}, p])
         res.update(ar['result'][0])
         mydata = bson.json_util.dumps(res)
-        return flask.Response(mydata,  mimetype='application/json')
+        return flask.Response(mydata, mimetype='application/json')
     except:
         return flask.make_response(ERROR_USER_NOT_FOUND, 404)
 
@@ -134,6 +172,7 @@ def get_user_fast(user_id):
 def user_list_output(user_ids, offset, limit):
     users = map(get_user, user_ids)
     return bson.json_util.dumps({'offset': offset, 'limit': limit, 'users': users})
+
 
 @app.route('/user/find')
 def search_user():
